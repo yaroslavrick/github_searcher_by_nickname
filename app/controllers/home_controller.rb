84f343622ef499c00a6098097b36feb662848ee7
Yaroslav Yenkala <yaroslavrick@gmail.com>
2022-08-01 10:51:43 +0300

    Added search engine and search veaw. App reuslt search now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,25 @@
 class HomeController < ApplicationController
-    # TODO
+  require 'faraday'
+  require 'faraday/net_http'
+  LINK = 'https://api.github.com/users/'.freeze
+
+  def search
+    @user_name = params[:name]
+    github_user_name
+    github_user_repos
+  end
+
+  private
+
+  def github_user_name
+    response_get_name = Faraday.get("#{LINK}#{@user_name}")
+    @github_name = JSON.parse(response_get_name.body)['name']
+    @github_name
+  end
+
+  def github_user_repos
+    response_get_repos = Faraday.get("#{LINK}#{@user_name}/repos")
+    @github_repos = JSON.parse(response_get_repos.body).map { |repo| repo['name'] }
+    @github_repos
+  end
 end
