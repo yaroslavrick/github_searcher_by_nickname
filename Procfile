39fc840326c1ad112baf7b98f50376f60c13f273
Yaroslav <yaroslavrick@gmail.com>
2023-09-17 22:41:22 +0300

    Add Procfile and config.hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bin/rails db:migrate && yarn build:css && yarn build && bin/rails server -b 0.0.0.0 -p ${PORT:-3000}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << ENV.fetch('DEVELOPMENT_HOSTS', nil)
   # Code is not reloaded between requests.
   config.cache_classes = true
 
