5a485c33d2d68aa159bad7cd2b98fc5a9d68d547
Yaroslav <yaroslavrick@gmail.com>
2023-09-24 22:14:15 +0300

    Add locale switch to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
-  resources :home, only: [:index]
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    root 'home#index'
+    resources :home, only: [:index]
+  end
 end
